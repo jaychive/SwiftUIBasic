6f1d5a856616ba6a5c1e898605255dedc6da6541
jaychive <shuga6@naver.com>
2025-08-05 16:37:24 +0900

    Initial Commit

---
+++ SwiftUIBasicTests/SwiftUIBasicTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBasicTests.swift
+//  SwiftUIBasicTests
+//
+//  Created by JaymeAir on 8/5/25.
+//
+
+import Testing
+@testable import SwiftUIBasic
+
+struct SwiftUIBasicTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
